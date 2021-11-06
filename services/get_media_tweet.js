37e1d88e96c0a867a835b4f4a9d187bfcd493eb9
Kartik Bansal <kartikbansal2000@gmail.com>
2021-11-06 11:51:52 +0530

    Optimised

--- services/get_media_tweet.js
+++ services/get_media_tweet.js
@@ -1,15 +1,14 @@
 const { response } = require("express");
 var axios = require("axios");
-const vision = require('@google-cloud/vision');
-var rgb2hex = require('rgb2hex');
+const vision = require("@google-cloud/vision");
+var rgb2hex = require("rgb2hex");
 const client = new vision.ImageAnnotatorClient({
-  keyFilename: './vision-creds.json'
+  keyFilename: "./vision-creds.json",
 });
 const { replyTweet } = require("./twitter");
 
-
-
 const getTag = async (image, id) => {
+  console.log(id);
   var data = JSON.stringify({
     records: [
       {
@@ -34,54 +33,64 @@ const getTag = async (image, id) => {
   };
 
   axios(config)
-    .then(async (response) =>{
-      // console.log(JSON.stringify(response.data));
-      // console.log(response.data['records'][0])
-      // console.log("------------------------")
-    //   console.log(response.data['records'][0]['_objects'][0]['_tags'])   
-      // console.log("------------------------")
-      // console.log(response.data['records'][0]['_objects'][0]['_tags'])
-      // console.log(response.data['records'][0]['_objects'][0]['_tags']['Category'][0]['name'])
-      const category = response.data['records'][0]['_objects'][0]['_tags']['Category'][0]['name']
-      const subcategory = response.data['records'][0]['_objects'][0]['_tags']['Subcategory'][0]['name']
-      const color = response.data['records'][0]['_objects'][0]['_tags']['Color'][0]['name']
-      const gender = response.data['records'][0]['_objects'][0]['_tags']['Gender'][0]['name']
-      const neckline = response.data['records'][0]['_objects'][0]['_tags']['Neckline'][0]['name']
-      const sleeves = response.data['records'][0]['_objects'][0]['_tags']['Sleeves'][0]['name']
-      const material = response.data['records'][0]['_objects'][0]['_tags']['Material'][0]['name']
-      const style = response.data['records'][0]['_objects'][0]['_tags']['Style'][0]['name']
-      const age = response.data['records'][0]['_objects'][0]['_tags']['Age'][0]['name']
-      const design = response.data['records'][0]['_objects'][0]['_tags']['Design'][0]['name']
-      const top_category = response.data['records'][0]['_objects'][0]['_tags']['Top Category'][0]['name']
+    .then(async (response) => {
+      const category =
+        response.data["records"][0]["_objects"][0]["_tags"]["Category"][0][
+          "name"
+        ];
+      const subcategory =
+        response.data["records"][0]["_objects"][0]["_tags"]["Subcategory"][0][
+          "name"
+        ];
+      const color =
+        response.data["records"][0]["_objects"][0]["_tags"]["Color"][0]["name"];
+      const gender =
+        response.data["records"][0]["_objects"][0]["_tags"]["Gender"][0][
+          "name"
+        ];
+      //   const neckline = response.data['records'][0]['_objects'][0]['_tags']['Neckline'][0]['name']
+      //   const sleeves = response.data['records'][0]['_objects'][0]['_tags']['Sleeves'][0]['name']
+      //   const material = response.data['records'][0]['_objects'][0]['_tags']['Material'][0]['name']
+      //   const style = response.data['records'][0]['_objects'][0]['_tags']['Style'][0]['name']
+      //   const age = response.data['records'][0]['_objects'][0]['_tags']['Age'][0]['name']
+      //   const design = response.data['records'][0]['_objects'][0]['_tags']['Design'][0]['name']
+      //   const top_category = response.data['records'][0]['_objects'][0]['_tags']['Top Category'][0]['name']
+      console.log(gender)
+      newSubCategory = subcategory.replace(" ","-")
+      console.log(newSubCategory)
+      console.log(color)
+      if(color == "Multicolor"){
+        
+      }
+      const hex = await detectColour(image);
 
-      const hex = await detectColour(image)
+      const myntra_url = `https://www.myntra.com/${gender}-${newSubCategory}?f=Color%3A${capitalizeFirstLetter(
+        color
+      )}_${hex}&sort=popularity`;
+      console.log(myntra_url);
 
-      const myntra_url = `https://www.myntra.com/${gender}-${subcategory}?f=Color%3A${capitalizeFirstLetter(color)}_${hex}`
-      console.log(myntra_url)
+      replyTweet("Hey we found your product here " + myntra_url, id)
+        .then((tweet) => {
+          // console.log(`tweet #1 ==>`, tweet)
+        })
+        .catch((error) => console.log(`error ==>`, error));
 
-      replyTweet('Hey we found your product here '+myntra_url, id)
-      .then(tweet => {
-
-        // console.log(`tweet #1 ==>`, tweet)
-      })
-      .catch(error => console.log(`error ==>`, error))
-
-      return myntra_url
+      return myntra_url;
     })
     .catch(function (error) {
       console.log(error);
     });
 };
 
-const detectColour = async(file_name)=>{
+const detectColour = async (file_name) => {
   const [result] = await client.imageProperties(file_name);
   red = result.imagePropertiesAnnotation.dominantColors.colors[1].color.red;
   green = result.imagePropertiesAnnotation.dominantColors.colors[1].color.green;
   blue = result.imagePropertiesAnnotation.dominantColors.colors[1].color.blue;
   // console.log(rgb2hex(`rgba(${red},${green},${blue})`).hex);
-  var hexcode = rgb2hex(`rgba(${red},${green},${blue})`).hex
+  var hexcode = rgb2hex(`rgba(${red},${green},${blue})`).hex;
   return hexcode.substring(1);
-}
+};
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
